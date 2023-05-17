e99d33d38eb039c23ff8c35cd8796d518f4c0f4e
vc80 <vic.p.cao@newegg.com>
2023-05-17 09:43:05 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <h1>🎙️ Vue.js Dictaphone</h1>
+
+    <template v-if="showError">
+      Your browser doesn’t support audio recording or you’ve blocked microphone access.
+    </template>
+
+    <template v-else>
+      <dictaphone @stop="handleRecording($event)" @error="handleError" mime-type="audio/mp3">
+        <template slot-scope="{ isRecording, startRecording, stopRecording }">
+          <button v-if="!isRecording" @click="startRecording">Start recording</button>
+          <button v-else @click="stopRecording">Stop recording</button>
+          <spectrum-analyser :style="{ opacity: isRecording ? 1 : .5, display:'block' }"/>
+        </template>
+      </dictaphone>
+
+      <template v-if="audioSource">
+        <audio :src="audioSource" controls></audio>
+      </template>
+    </template>
   </div>
 </template>
 
 <script>
+
+import Dictaphone from '@/components/Dictaphone';
+import SpectrumAnalyser from '@/components/SpectrumAnalyser';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: 'app',
+  data() {
+    return {
+      audioSource: null,
+      showError: false,
+    };
+  },
+  methods: {
+    handleError() {
+      this.showError = true;
+    },
+    // eslint-disable-next-line no-unused-vars
+    handleRecording({ blob, src }) {
+      this.audioSource = src;
+    },
+  },
+  components: {
+    Dictaphone,
+    SpectrumAnalyser,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 </style>
